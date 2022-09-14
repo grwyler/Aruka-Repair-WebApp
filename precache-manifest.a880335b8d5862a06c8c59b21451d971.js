03d0aa113be717ec440f8c49722d445e537b16a5
Garrett Wyler <grwyler@gmail.com>
2022-09-14 08:33:37 -0400

    Updates

---
+++ precache-manifest.a880335b8d5862a06c8c59b21451d971.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82be37d75ae8e5ecdb70b1a5f4252b7d",
+    "url": "/Aruka-Repair-WebApp/index.html"
+  },
+  {
+    "revision": "78589c04f8f07d38d3d9",
+    "url": "/Aruka-Repair-WebApp/static/css/main.4ecbd5e7.chunk.css"
+  },
+  {
+    "revision": "393faab25e91b802a0d3",
+    "url": "/Aruka-Repair-WebApp/static/js/2.736ddffb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Aruka-Repair-WebApp/static/js/2.736ddffb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78589c04f8f07d38d3d9",
+    "url": "/Aruka-Repair-WebApp/static/js/main.7c08f9eb.chunk.js"
+  },
+  {
+    "revision": "ffc2319a1f64ffce0670",
+    "url": "/Aruka-Repair-WebApp/static/js/runtime-main.953a169c.js"
+  }
+]);
